b109e52994ba9e3ba5d07a7da46af0f49d7044bd
Jeroen-TheCodingFactory <tcf@jeroen-rijsdijk.nl>
2022-11-23 15:43:50 +0100

    refactored to new version of react router and hooks

--- src/QuestionsRouter/QuestionsRouter.js
+++ src/QuestionsRouter/QuestionsRouter.js
@@ -1,26 +1,14 @@
 import Question from "../Question/Question";
 import questionsFromData from "../data/questions";
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const QuestionsRouter = (props) => {
     const [questions, setQuestions] = useState([]);
-    const [numberFromURL, setNumber] = useState(null);
-    const [questionToBeRendered, setQuestiontoBeRendered] = useState(null);
 
     useEffect(() => {
         setQuestions(questionsFromData);
-        setNumber(1);
-    }, [])
-
-    useEffect(() => {
-        let questionSearch = questions.map(questionObject => {
-            if (questionObject.number === numberFromURL) {
-                return <Question onLast={() => props.onLast(questions)} last={questionObject.last} onRate={onRate} previous={questionObject.previous} next={questionObject.next} number={questionObject.number} key={questionObject.number} question={questionObject.question} rating={questionObject.rating} />;
-            }
-        });
-
-        setQuestiontoBeRendered(questionSearch);
-    }, [numberFromURL])
+    }, []);
 
     const onRate = (rating, number) => {
         let oldState = questions;
@@ -33,10 +21,26 @@ const QuestionsRouter = (props) => {
         });
         setQuestions(newState);
     }
+    
+    const numberFromURL = useParams();
+
+    let questionSearch = questions.map(questionObject => {
+        if (questionObject.number == numberFromURL.number) {
+            return <Question onLast={() => props.onLast(questions)}
+                last={questionObject.last}
+                onRate={onRate}
+                previous={questionObject.previous}
+                next={questionObject.next}
+                number={questionObject.number}
+                key={questionObject.number}
+                question={questionObject.question}
+                rating={questionObject.rating} />;
+        }
+    });
 
     return (
         <>
-            {questionToBeRendered}
+            {questionSearch}
         </>
     )
 }
