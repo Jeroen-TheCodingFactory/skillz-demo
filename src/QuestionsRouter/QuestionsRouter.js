c028cef4792631d0252834e011c2d21b5dd4e97f
Jeroen-TheCodingFactory <tcf@jeroen-rijsdijk.nl>
2022-11-23 14:54:51 +0100

    refactored question and router to hooks

--- src/QuestionsRouter/QuestionsRouter.js
+++ src/QuestionsRouter/QuestionsRouter.js
@@ -1,45 +1,44 @@
-import React from "react";
 import Question from "../Question/Question";
-import {withRouter} from "react-router-dom";
-import questions from "../data/questions";
+import questionsFromData from "../data/questions";
+import { useState, useEffect } from "react";
 
-class QuestionsRouter extends React.Component{
-    constructor(props){
-        super(props);
-        this.myNumber = this.props.match.params.number;
-        this.state = {questions: []}
-    }
-    componentDidMount(){
-        this.setState({ questions: questions});
-    }
+const QuestionsRouter = (props) => {
+    const [questions, setQuestions] = useState([]);
+    const [numberFromURL, setNumber] = useState(null);
+    const [questionToBeRendered, setQuestiontoBeRendered] = useState(null);
 
-    onRate = (rating,number) =>{
-       let oldState = this.state.questions;
-       let newState = oldState.map( question => {
-            if(question.number === number ){
+    useEffect(() => {
+        setQuestions(questionsFromData);
+        setNumber(1);
+    }, [])
+
+    useEffect(() => {
+        let questionSearch = questions.map(questionObject => {
+            if (questionObject.number === numberFromURL) {
+                return <Question onLast={() => props.onLast(questions)} last={questionObject.last} onRate={onRate} previous={questionObject.previous} next={questionObject.next} number={questionObject.number} key={questionObject.number} question={questionObject.question} rating={questionObject.rating} />;
+            }
+        });
+
+        setQuestiontoBeRendered(questionSearch);
+    }, [numberFromURL])
+
+    const onRate = (rating, number) => {
+        let oldState = questions;
+        let newState = oldState.map(question => {
+            if (question.number === number) {
                 question.rating = rating;
                 return question;
             }
             return question;
-       });
-
-      this.setState({question: newState});
-    }
-    
-
-    render(){
-        this.myNumber = this.props.match.params.number;
-        let questionToBeRendered = this.state.questions.map( questionObject => {
-                if(questionObject.number == this.myNumber){
-                    return <Question onLast={() => this.props.onLast(this.state.questions)} last={questionObject.last} onRate={this.onRate} previous={questionObject.previous} next={questionObject.next} number={questionObject.number} key={questionObject.number} question={questionObject.question} rating={questionObject.rating}/>;
-                }
         });
-        return (
-            <>
-                {questionToBeRendered}
-            </>
-        )
+        setQuestions(newState);
     }
+
+    return (
+        <>
+            {questionToBeRendered}
+        </>
+    )
 }
 
-export default withRouter(QuestionsRouter);
+export default QuestionsRouter;
